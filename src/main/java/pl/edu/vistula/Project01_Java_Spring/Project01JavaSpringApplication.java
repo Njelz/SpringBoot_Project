e6c0c5b8288d5b16d7dc63288af2f164282b5688
Tafadzwa Njela <tafanjela5@gmail.com>
2026-01-13 19:36:52 +0100

    Initial commit

---
+++ src/main/java/pl/edu/vistula/Project01_Java_Spring/Project01JavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.Project01_Java_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project01JavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project01JavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/Project01_Java_Spring/Project01JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.Project01_Java_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project01JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
